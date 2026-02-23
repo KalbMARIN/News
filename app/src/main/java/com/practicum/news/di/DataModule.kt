127ad5207a9867ea16f6d98e2ed2ec1676940d75
artembelonozhko <ar.belonozhko@gmail.com>
2026-02-23 17:57:54 +0300

    implement repository

--- app/src/main/java/com/practicum/news/di/DataModule.kt
+++ app/src/main/java/com/practicum/news/di/DataModule.kt
@@ -5,6 +5,9 @@ import androidx.room.Room
 import com.practicum.news.data.local.NewsDao
 import com.practicum.news.data.local.NewsDatabase
 import com.practicum.news.data.remote.NewsApiService
+import com.practicum.news.data.repository.NewsRepositoryImpl
+import com.practicum.news.domain.repository.NewsRepository
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -21,6 +24,12 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 interface DataModule {
+
+    @Binds
+    @Singleton
+    fun bindNewsRepository(
+        impl: NewsRepositoryImpl
+    ): NewsRepository
 
     companion object {
 
--- app/src/main/java/com/practicum/news/presentation/MainActivity.kt
+++ app/src/main/java/com/practicum/news/presentation/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.practicum.news.presentation
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -11,14 +12,27 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.lifecycleScope
+import com.practicum.news.data.remote.NewsApiService
+import com.practicum.news.domain.repository.NewsRepository
 import com.practicum.news.presentation.ui.theme.NewsTheme
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var repository: NewsRepository
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        lifecycleScope.launch {
+            repository.addSubscription("Kotlin")
+            repository.updateArticlesForTopic("Kotlin")
+        }
         setContent {
             NewsTheme {
 
