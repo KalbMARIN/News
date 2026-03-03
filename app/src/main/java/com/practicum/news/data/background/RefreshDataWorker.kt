042f03dfd20b1b58ea559a745477a2cd37eb41b7
artembelonozhko <ar.belonozhko@gmail.com>
2026-03-03 16:26:47 +0300

    implement Worker

--- app/src/main/java/com/practicum/news/data/background/RefreshDataWorker.kt
+++ app/src/main/java/com/practicum/news/data/background/RefreshDataWorker.kt
@@ -2,13 +2,17 @@ package com.practicum.news.data.background
 
 import android.content.Context
 import android.util.Log
+import androidx.hilt.work.HiltWorker
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import com.practicum.news.domain.usecase.UpdateSubscribedArticlesUseCase
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedInject
 
-class RefreshDataWorker(
-    context: Context,
-    workerParameters: WorkerParameters,
+@HiltWorker
+class RefreshDataWorker @AssistedInject constructor(
+    @Assisted context: Context,
+    @Assisted workerParameters: WorkerParameters,
     private val updateSubscribedArticlesUseCase: UpdateSubscribedArticlesUseCase
 ) : CoroutineWorker(context, workerParameters) {
 
